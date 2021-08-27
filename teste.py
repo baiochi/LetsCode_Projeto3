44b69353337ed5047548e8d9aeca5e8731769b49
WillFelipeRS <wf.rodrigues@yahoo.com.br>
2021-08-27 20:56:36 -0300

    Primeiras alterações

--- teste.py
+++ teste.py
@@ -1,14 +1,21 @@
 #first file
 
-from datatime import datetime.date as dtd
-from datatime import datetime.time as dtt
+from datetime import datetime.date as dtd
+from datetime import datetime.time as dtt
 
 class Cliente(object):
+    # Ver as bicicletas disponíveis na Loja;
+    # Alugar bicicletas por hora (R$5/hora);
+    # Alugar bicicletas por dia (R$25/dia);
+    # Alugar bicicletas por semana (R$100/semana)
+    # Aluguel para família, uma promoção que pode incluir de 3 a 5 empréstimos
+    # (de qualquer tipo) com 30% de desconto no valor total.
+
     def __init__(self, nome):
         self.cliente = nome
 
-    def disponivel(self, nome):
-       print(nome.estoque)
+    def disponivel(self, loja):
+       print(loja.estoque)
 
     def alugar(self, loja):
         try:
@@ -17,7 +24,7 @@ class Cliente(object):
                 raise ValueError
                 
             familiar = input('É um aluguel familiar? ')
-            data 
+            # data 
             
         except ValueError:
             print('msg erro')
@@ -27,22 +34,17 @@ class Cliente(object):
     def devolver(self, horaDevolucao, quantidade, familiar):
         pass
     
-
-
 class Loja(object):
-    def __init__(self, nome, endereco, estoque):
-        self.loja = nome
-        self.endereco = endereco
-        self.estoque = estoque
-
+    def __init__(self):
+        self.estoque = 100
     
-
-
-
+        # Calcular a conta quando o cliente decidir devolver a bicicleta;
+        # Mostrar o estoque de bicicletas;
+        # Receber pedidos de aluguéis por hora, diários ou semanais validando a possibilidade com o estoque.
+    
+        
 # COMPRA DE BICICLETAS
 
 lojaMatriz = Loja('Bicicletas Paraiso', 'Rua Nascer do sol', 5)
 cliente = Cliente('jose')
 cliente.disponivel(lojaMatriz)
-
-    
