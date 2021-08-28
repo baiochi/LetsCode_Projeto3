9f284e96ab42a6dfd90e5df3951da08629e92d11
WillFelipeRS <wf.rodrigues@yahoo.com.br>
2021-08-28 19:10:21 -0300

    Atualização - 19:10

--- main Will.py
+++ main Will.py
@@ -11,12 +11,12 @@ class Loja (object):
         return self.estoque
 
     # Alterar a quantidade do estoque.
-    def alterarEstoque (self, estoque):
-        self.estoque = estoque
+    def alterarEstoque (self, baixa):
+        self.estoque -= baixa
 
     # Receber pedidos de aluguéis por hora, diários ou semanais validando a disponibilidade do estoque.
-    def receberPedido (self, estoqueAtual, pedido):
-        self.estoqueAtual = estoqueAtual
+    def receberPedido (self, pedido):
+        self.estoqueAtual = loja.mostrarEstoque()
         self.pedido = pedido
 
     # Calcular a conta quando o cliente decidir devolver a bicicleta;
@@ -43,8 +43,10 @@ class Cliente (object):
     
     # Aluguel para família, uma promoção que pode incluir de 3 a 5 empréstimos
     # (de qualquer tipo) com 30% de desconto no valor total.
-    def alugarFamilia (self, quantidade, *opcaoDesejada):
-        pass
+    def alugarFamilia (self, quantidade, tipoAluguel, periodo):
+        self.quantidade = quantidade
+        self.tipoAluguel = tipoAluguel
+        self.periodo = periodo
 
 # Função para validar a entrada de dados do input do cliente:
 # Argumentos: entrada
@@ -63,34 +65,55 @@ def menuEntradas(entrada):
         print(f'O estoque atual é de {loja.mostrarEstoque()} biciletas disponíveis.')
         entrada = validaEntrada(input('\nQual o plano que você deseja contratar? ').upper())
         menuEntradas(entrada)
+        return entrada
 
-    elif entrada == '0':
+    elif entrada == '0': # Está printando "None".
         print('\nÉ uma pena... Mas aguardamos você numa próxima!')
-
+        
     # Plano Hora
     elif entrada == '1':
         print('\nO plano \"Dar uma voltinha\" foi selecionado!')
         qtdHoras = input('Por quantas horas você deseja alugar? ')
         cliente.alugarHora(qtdHoras)
+        return entrada
 
     # Plano Dia
     elif entrada == '2':
         print('\nO plano Diário foi selecionado!')
         qtdDias = input('Por quantos dias você deseja alugar? ')
         cliente.alugarDia(qtdDias)
+        return entrada
 
     # Plano Semana
     elif entrada == '3':
         print('\nO plano Semanal foi selecionado!')
         qtdSemanas = input('Por quantas semanas você deseja alugar? ')
         cliente.alugarSemana(qtdSemanas)
+        return entrada
 
     # Plano Família
     else: # opcao == '4'
         print('\nO plano Família foi selecionado!')
+        qtdBicicletas = input('Quantas bicicletas você deseja alugar? ')
+        while qtdBicicletas not in ('3','4','5'):
+            qtdBicicletas = input('Digite um valor entre 3 e 5! Quantas bicicletas você deseja alugar? ')
+        plano = input(f'Em qual dos planos você deseja alugar {qtdBicicletas} bicicletas? ')
+        while plano not in ('1','2','3'):
+            print('Valor Inválido! Digite 1, 2 ou 3 correspondente ao plano desejado.')
+            plano = input(f'Em qual dos planos você deseja alugar {qtdBicicletas} bicicletas? ')
+        preenche = ""
+        if plano == '1':
+            preenche = 'quantas horas'
+        elif plano == '2':
+            preenche = 'quantos dias'
+        else:
+            preenche = 'quantas semanas'
+        tempo = input(f'Por {preenche} você deseja alugar {qtdBicicletas} bicicletas? ')
+        cliente.alugarFamilia(qtdBicicletas, plano, tempo)
+        return entrada, qtdBicicletas, plano, tempo
 
 
-# Inicializacao das class
+# Init classes
 cliente = Cliente(1000)
 loja = Loja(100)
 
@@ -107,6 +130,9 @@ print('digite \"E\" para visualizar a quantidade de biciletas em estoque')
 print('ou digite 0 para cancelar.')
 
 opcao = validaEntrada(input('\nQual o plano que você deseja contratar? ').upper())
-menuEntradas(opcao)
+pedidoCliente = (menuEntradas(opcao))
+
+# Log
+print(f'A opção do cliente foi: {pedidoCliente}')
 
 print('\nFim do programa.')
