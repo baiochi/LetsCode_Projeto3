b4b2fb1a09af9e13b4a633369697f44472769c89
Joao Baiochi <jao.baiochi@gmail.com>
2021-08-28 18:24:09 -0300

    classe Cliente método alugarBicicleta atualizado classe Loja método set/get atualizado

--- main_joao.py
+++ main_joao.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import datetime as dt
 from datetime import time
 
 """
@@ -15,28 +15,40 @@ class Cliente(object):
 
     def __init__(self) -> None:
         self.modeloAluguel = ''
+        self.qtdeBicicletas = 0
+        self.promocaoFamilia = False
+        self.dataAluguel = dt.today()
 
-    # 1 - Ver as bicicletas disponíveis na Loja;
+    # Ver as bicicletas disponíveis na Loja;
     def verBiciletas(self, loja):
         return loja.estoque
 
-    #- Alugar bicicletas por hora (R$5/hora), dia (R$25/dia), semana (R$100/semana)
-    def alugarBicicleta(self, loja, quantidade, modeloAluguel):
+    # Alugar bicicletas por hora (R$5/hora), dia (R$25/dia), semana (R$100/semana)
+    # Opcional: promoção que pode incluir de 3 a 5 aluguéis com 30% de desconto no valor total.
+    def alugarBicicleta(self, loja, quantidade, modeloAluguel, promocaoFamilia = False):
         try:
+            # Verificar o estoque
             if quantidade > loja.estoque:
                 raise ValueError('Estoque insuficiente.')
-            if modeloAluguel in ['dia', 'hora', 'semana']:
-                self.modeloAluguel = modeloAluguel
-                print(f'Aluguel por R${loja.tabelaPrecos[modeloAluguel]}/{modeloAluguel}')
-            else:
+            # Validar o modelo do aluguel
+            if modeloAluguel not in ['dia', 'hora', 'semana']:
                 raise Exception('Modelo de alguel inválido.')
+            # Validar da promoção
+            if promocaoFamilia and not 3 <= quantidade <= 5:
+                self.promocaoFamilia = False # reseta a varíável para evitar bug em chamadas futuras
+                raise Exception('Quantidade inválida para validar a promoção.')
+
+            loja.aumentarEstoque(quantidade)
+            self.qtdeBicicletas = quantidade # atualiza quantidade de bicicletas que o cliente alugou
+            self.modeloAluguel = modeloAluguel # define o modelo do empréstimo
+            self.dataAluguel = dt.today() # registra a data/horário do aluguel
+            print(f'{quantidade} bicileta(s) alugada(s) por R${loja.tabelaPrecos[modeloAluguel]}/{modeloAluguel}\
+                 as {self.dataAluguel.strftime("%H:%M:%S")} no dia {self.dataAluguel.strftime("%d/%m/%y")}') #log
+
         except ValueError:
-            print('Modelo de alguel inválido')
-
-
-    # 5 - Aluguel para família, uma promoção que pode incluir de 3 a 5 empréstimos (de qualquer tipo) 
-    # com 30% de desconto no valor total.
-
+            print('Estoque insuficiente.')
+        except Exception:
+            print('Dado inválido.')
 
 """
 Loja pode:
@@ -49,21 +61,45 @@ class Loja(object):
 
     def __init__(self, estoque):
         self.estoque = estoque
-        self.tabelaPrecos ={
+        self.tabelaPrecos = {
             'hora' : 5,
             'dia' : 25,
             'semana' : 100
         }
     
-    # - Mostrar o estoque de bicicletas;
+    # Mostrar o estoque de bicicletas;
     def mostrarEstoque(self):
         return self.estoque
 
+    # Adiciona mais bicicletas ao estoque
+    def aumentarEstoque(self, estoque):
+        self.estoque += estoque
+
+    # Remove bicicletas do estoque
+    def diminuirEstoque(self, estoque):
+        self.estoque -= estoque
+
     # - Calcular a conta quando o cliente decidir devolver a bicicleta;
     def calcularConta(self, cliente):
+        if cliente.modeloAluguel == 'hora':
+            # Calcular a hora
+            tempoAluguel = cliente.dataAluguel
 
+
+        if cliente.modeloAluguel == 'dia':
+            pass
+        if cliente.modeloAluguel == 'semana':
+            pass
         pass
 
     #- Receber pedidos de aluguéis por hora, diários ou semanais validando a possibilidade com o estoque.
     def receberPedido():
         pass
+
+
+loja01 = Loja()
+
+loja01.tabelaPrecos
+loja01.alterarEstoque(100)
+
+
