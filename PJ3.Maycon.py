2889ed610c4ba1adfdeeefa18c91e484a6ddd43d
MAYCON D S MENDES <85529914+maycmesquia@users.noreply.github.com>
2021-08-29 00:33:51 -0300

    atualizacao

--- PJ3.Maycon.py
+++ PJ3.Maycon.py
@@ -2,7 +2,7 @@ import datetime
 
 class Cliente(object):
     def __init__(self, nome):
-        self.cliente = nome
+        self.nome = nome
     
     # Ver as bicicletas disponíveis na Loja;
     def disponivel(self, loja):
@@ -24,11 +24,12 @@ class Cliente(object):
             dia = int(input('Informe o dia do aluguel: '))
             self.horaInicial = int(input('Informe a hora Inicial: '))
             self.dataInicial = datetime.datetime(2021, mes, dia)
+            loja.estoque -= self.quantidadeAlugada
         except ValueError:
             print('Você deseja alugar {} bicicletas, mas a loja só tem {}'.format(self.quantidadeAlugada, loja.estoque))
             self.alugar(loja)               
     
-    def devolver(self, loja):
+    def devolver(self, loja, cliente):
         try:
             self.quantidadeDevolvida = int(input('Informe a quantidade de bicicletas que deseja devolver: '))
             if self.quantidadeDevolvida > self.quantidadeAlugada:
@@ -39,21 +40,20 @@ class Cliente(object):
             self.dataFinal = datetime.datetime(2021, mes, dia)
             loja.conta(cliente)
             self.quantidadeAlugada -= self.quantidadeDevolvida
+            loja.estoque += self.quantidadeDevolvida
         except ValueError:
-            print('Você alugou: {} bicicletas, e deseja devolver {}. informe um valor valido'.format(self.quantidadeAlugada, self.quantidadeDevolvida)
-
-
-
+            print('Você alugou: {} bicicletas, e deseja devolver {}.\
+                 informe um valor valido'.format(self.quantidadeAlugada, self.quantidadeDevolvida))
 
 
 class Loja(object):
 
 
     def __init__(self):
-        self.estoque = 100
+        self.estoque = 15
     
 
-    def conta(self, cliente, quantidade):
+    def conta(self, cliente):
 
         if cliente.horaFinal < cliente.horaInicial:
             horas = cliente.horaFinal
@@ -68,7 +68,7 @@ class Loja(object):
             horas = 0
         semanas = dias // 7
         semanasAluguel = diasAluguel // 7
-        dias = dias % 7
+        dias = dias%7
         diasAluguel = diasAluguel % 7
         if dias >= 4:
             semanas += 1
@@ -76,12 +76,12 @@ class Loja(object):
 
         fatorCobranca = (semanas * 100) + (dias * 25) + (horas * 5)
         if cliente.quantidadeAlugada > 3 and cliente.quantidadeAlugada < 5:
-            descontoFamilia = 30%
+            descontoFamilia = 0,3
         else:
-            descontoFamilia = 0%
+            descontoFamilia = 0
         valorCobranca = fatorCobranca * cliente.quantidadeDevolvida
         valorDesconto = valorCobranca * descontoFamilia
-        valorPagar = valorCobranca - DescontoFamilia
+        valorPagar = valorCobranca - valorDesconto
         
         print(f'Você devolveu {cliente.quantidadeDevolvida} bicicletas, de um total de {cliente.quantidadeAlugada} bicicletas alugadas!')
         print(f'Periodo de locação: {semanasAluguel} semanas, {diasAluguel} dias e {horasAluguel} horas')
@@ -97,7 +97,17 @@ class Loja(object):
 # Simulação de alugueis:
 
 
+print('Criando Loja e Clientes!')
 matriz = Loja()
-clienteA = Cliente()
-clienteA.disponivel(matriz)
-clienteA.alugar
+clienteA = Cliente('Sr.Zé')
+clienteB = Cliente('Sr.Jão')
+#print('Consulta de estoque e aluguel Sr.Zé!')
+#clienteA.disponivel(matriz)
+#clienteA.alugar(matriz)
+#print('Consulta de estoque e aluguel Sr.Jão!')
+#clienteB.disponivel(matriz)
+#clienteB.alugar(matriz)
+print('Devolução de bicicletas Sr.Zé e Sr.Jão')
+clienteA.devolver(matriz, clienteA)
+clienteB.disponivel(matriz)
+clienteB.devolver(matriz, clienteB)
