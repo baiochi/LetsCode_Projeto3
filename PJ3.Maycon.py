d68e11a02077c0d5d0eda094f8998e7739e21ce1
MAYCON D S MENDES <85529914+maycmesquia@users.noreply.github.com>
2021-08-28 21:41:56 -0300

    alteraçãoes Maycon

--- PJ3.Maycon.py
+++ PJ3.Maycon.py
@@ -1,7 +1,4 @@
-!pip install datetime
-import time
-from datetime import date
-
+import datetime
 
 class Cliente(object):
     def __init__(self, nome):
@@ -19,12 +16,15 @@ class Cliente(object):
     # (de qualquer tipo) com 30% de desconto no valor total.
 
     def alugar(self, loja):
-        try:
+        try: 
             self.quantidadeAlugada = int(input('Informe a quantidade de bicicletas que deseja alugar: '))
             if self.quantidadeAlugada > loja.estoque:
                 raise ValueError
-            self.dataInicial = datetime.today()
-            self.horaInicial = int(input('Informe a hora do aluguel: '))  
+            mes = int(input('Informe o mês do aluguel: '))
+            dia = int(input('Informe o dia do aluguel: '))
+            self.dataInicial = datetime.date(2021, mes, dia)
+            hora = int(input('Informe a hora Inicial: '))
+            self.horaInicial = datetime.time(hora, 0)  
         except ValueError:
             print('Você deseja alugar {} bicicletas, mas a loja só tem {}'.format(self.quantidadeAlugada, loja.estoque))
             self.alugar(loja)               
@@ -34,8 +34,11 @@ class Cliente(object):
             self.quantidadeDevolvida = int(input('Informe a quantidade de bicicletas que deseja devolver: '))
             if self.quantidadeDevolvida > self.quantidadeAlugada:
                 raise ValueError
-            self.dataFinal = datetime.today()
+            mes = int(input('Informe o mês da devolução: '))
+            dia = int(input('Informe o dia da devolução: '))
+            self.dataFinal = datetime.date(2021, mes, dia)
             self.horaFinal = int(input('Informe a hora da devolução: '))
+            loja.conta('informar parametros necessarios')
         except ValueError:
             print('Você alugou: {} bicicletas, e deseja devolver {}. informe um valor valido'.format(self.quantidadeAlugada, self.quantidadeDevolvida)
 
@@ -44,13 +47,27 @@ class Loja(object):
     def __init__(self):
         self.estoque = 100
     
+    def conta(self, cliente):
+        horas = cliente.horaFinal - cliente.horaInicial
+        dias = cliente.dataFinal - cliente.dataInicial
+        dias += horas // 24
+        if horas > 5:
+            dias += 1
+        semanas = dias // 7
+            
+
+    
+
+        quantidade = cliente.quantidadeDevolvida
+        valorAPagar = 
         # Calcular a conta quando o cliente decidir devolver a bicicleta;
         # Mostrar o estoque de bicicletas;
         # Receber pedidos de aluguéis por hora, diários ou semanais validando a possibilidade com o estoque.
     
         
-# COMPRA DE BICICLETAS
+# Simulação de alugueis:
+
 
 Matriz = Loja()
-cliente = Cliente('jose')
-cliente.disponivel(lojaMatriz)
+cliente1 = Cliente('jose')
+cliente.disponivel(Matriz)
