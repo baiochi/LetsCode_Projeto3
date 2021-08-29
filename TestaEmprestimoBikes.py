c0037e4d4c9456263d032ae416ec82c108942edd
Joao Baiochi <jao.baiochi@gmail.com>
2021-08-29 12:33:10 -0300

    iniciando o arquivo de testes

--- TestaEmprestimoBikes.py
+++ TestaEmprestimoBikes.py
@@ -1,1 +1,14 @@
 # ambiente de testes unitários
+import unittest
+from class_joao import Cliente, Loja
+from datetime import datetime as dt
+
+class ClienteTests(unittest.TestCase):
+    pass
+
+class LojaTests(unittest.TestCase):
+    pass
+
+# executa todos os testes dentro das classes
+if __name__ == '__main__':
+    unittest.main()
