ae95bedcf01cc30e6558e2bdd3117e5976a5632c
WillFelipeRS <wf.rodrigues@yahoo.com.br>
2021-08-28 21:32:24 -0300

    Atualização 21:30 - receberPedido e calcularConta em desenvolvimento.

--- main Will.py
+++ main Will.py
@@ -15,102 +15,117 @@ class Loja (object):
         self.estoque -= baixa
 
     # Receber pedidos de aluguéis por hora, diários ou semanais validando a disponibilidade do estoque.
-    def receberPedido (self, pedido):
-        self.estoqueAtual = loja.mostrarEstoque()
-        self.pedido = pedido
+    def receberPedido (self, periodo, *quantPlano):
+        pass
 
     # Calcular a conta quando o cliente decidir devolver a bicicleta;
-    def calcularConta (self, opcaoDesejada, pedido):
-        self.opcaoDesejada = opcaoDesejada
-        self.pedido = pedido
+    def calcularConta (self):
+        pass
 
 class Cliente (object):
     
     def __init__ (self, carteira):
         self.carteira = carteira
 
+    # Ver as bicicletas disponíveis na Loja;
+    def checarEstoque (self):
+        print(f'O estoque atual é de {loja.mostrarEstoque()} biciletas disponíveis.')
+        self.opcao = validaEntrada(input('\nQual o plano que você deseja contratar? ').upper())
+        menuEntradas(self.opcao)
+        return self.opcao
+    
     # Alugar bicicletas por hora (R$5/hora);
-    def alugarHora (self, periodo):
-        self.periodo = periodo
+    def alugarHora (self):
+        self.periodo = input('Por quantas horas você deseja alugar? ')
+        print(f'Selecionado {self.periodo} hora(s)!')
+        return self.periodo
     
     # Alugar bicicletas por dia (R$25/dia);
-    def alugarDia (self, periodo):
-        self.periodo = periodo
+    def alugarDia (self):
+        self.periodo = input('Por quantos dias você deseja alugar? ')
+        print(f'Selecionado {self.periodo} dia(s)!')
+        return self.periodo
 
     # Alugar bicicletas por semana (R$100/semana)
-    def alugarSemana (self, periodo):
-        self.periodo = periodo
+    def alugarSemana (self):
+        self.periodo = input('Por quantas semanas você deseja alugar? ')
+        print(f'Selecionado {self.periodo} semana(s)!')
+        return self.periodo
     
     # Aluguel para família, uma promoção que pode incluir de 3 a 5 empréstimos
     # (de qualquer tipo) com 30% de desconto no valor total.
-    def alugarFamilia (self, quantidade, tipoAluguel, periodo):
-        self.quantidade = quantidade
-        self.tipoAluguel = tipoAluguel
-        self.periodo = periodo
+    def alugarFamilia (self):
+        self.qtdBicicletas = input('Quantas bicicletas você deseja alugar? ')
+        while self.qtdBicicletas not in ('3','4','5'):
+            self.qtdBicicletas = input('Digite um valor entre 3 e 5! Quantas bicicletas você deseja alugar? ')
+        
+        self.plano = input(f'Em qual dos planos você deseja alugar {self.qtdBicicletas} bicicletas? ')
+        while self.plano not in ('1','2','3'):
+            print('Valor Inválido! Digite 1, 2 ou 3 correspondente ao plano desejado.')
+            self.plano = input(f'Em qual dos planos você deseja alugar {self.qtdBicicletas} bicicletas? ')
+        
+        preenche = ""
+        if self.plano == '1':
+            preenche = 'quantas horas'
+        elif self.plano == '2':
+            preenche = 'quantos dias'
+        else:
+            preenche = 'quantas semanas'
+        self.periodo = input(f'Por {preenche} você deseja alugar {self.qtdBicicletas} bicicletas? ')
+        
+        preenche2 = ""
+        if self.plano == '1':
+            preenche2 = 'hora(s)'
+        elif self.plano == '2':
+            preenche2 = 'dia(s)'
+        else:
+            preenche2 = 'semana(s)'
+        print(f'Selecionado alugar {self.qtdBicicletas} bicicletas no Plano {self.plano} por {self.periodo} {preenche2}!')
+        return self.periodo, self.qtdBicicletas, self.plano
 
-# Função para validar a entrada de dados do input do cliente:
-# Argumentos: entrada
-# Retorna: entrada
+'''
+Função para validar a entrada de dados do input do cliente:
+Argumentos: entrada
+Retorna: entrada
+'''
 def validaEntrada(entrada):
     _opcoes = ('0', '1', '2', '3', '4', 'E')
     while entrada not in _opcoes:
         entrada = input('\nOpção inválida! Qual o plano que você deseja contratar? ').upper()
     return entrada
 
-# Função do menu do cliente:
-# Argumentos: entrada
-# Retorna: 
+'''
+Função do menu: chama os devidos métodos de cada Classe de acordo com as escolhas do cliente.
+Argumentos: entrada
+Retorna: None.
+'''
 def menuEntradas(entrada):
     if entrada == 'E':
-        print(f'O estoque atual é de {loja.mostrarEstoque()} biciletas disponíveis.')
-        entrada = validaEntrada(input('\nQual o plano que você deseja contratar? ').upper())
-        menuEntradas(entrada)
-        return entrada
+        print('\nConferir o estoque disponível foi selecionado!')
+        cliente.checarEstoque()
 
-    elif entrada == '0': # Está printando "None".
+    elif entrada == '0':
         print('\nÉ uma pena... Mas aguardamos você numa próxima!')
         
     # Plano Hora
     elif entrada == '1':
-        print('\nO plano \"Dar uma voltinha\" foi selecionado!')
-        qtdHoras = input('Por quantas horas você deseja alugar? ')
-        cliente.alugarHora(qtdHoras)
-        return entrada
-
+        print('\nO plano Passeio foi selecionado!')
+        cliente.alugarHora()
+        
     # Plano Dia
     elif entrada == '2':
         print('\nO plano Diário foi selecionado!')
-        qtdDias = input('Por quantos dias você deseja alugar? ')
-        cliente.alugarDia(qtdDias)
-        return entrada
-
+        cliente.alugarDia()
+        
     # Plano Semana
     elif entrada == '3':
         print('\nO plano Semanal foi selecionado!')
-        qtdSemanas = input('Por quantas semanas você deseja alugar? ')
-        cliente.alugarSemana(qtdSemanas)
-        return entrada
-
+        cliente.alugarSemana()
+        
     # Plano Família
     else: # opcao == '4'
         print('\nO plano Família foi selecionado!')
-        qtdBicicletas = input('Quantas bicicletas você deseja alugar? ')
-        while qtdBicicletas not in ('3','4','5'):
-            qtdBicicletas = input('Digite um valor entre 3 e 5! Quantas bicicletas você deseja alugar? ')
-        plano = input(f'Em qual dos planos você deseja alugar {qtdBicicletas} bicicletas? ')
-        while plano not in ('1','2','3'):
-            print('Valor Inválido! Digite 1, 2 ou 3 correspondente ao plano desejado.')
-            plano = input(f'Em qual dos planos você deseja alugar {qtdBicicletas} bicicletas? ')
-        preenche = ""
-        if plano == '1':
-            preenche = 'quantas horas'
-        elif plano == '2':
-            preenche = 'quantos dias'
-        else:
-            preenche = 'quantas semanas'
-        tempo = input(f'Por {preenche} você deseja alugar {qtdBicicletas} bicicletas? ')
-        cliente.alugarFamilia(qtdBicicletas, plano, tempo)
-        return entrada, qtdBicicletas, plano, tempo
+        cliente.alugarFamilia()
 
 
 # Init classes
@@ -118,8 +133,9 @@ cliente = Cliente(1000)
 loja = Loja(100)
 
 print('Bem vindo à Locadora de Bicicletas LTDA!')
-print('Aqui temos uma variedade de planos para o aluguel de bicicletas, que são:')
-print('1 - Plano \"Dar uma voltinha\" - R$ 5,00 por hora;')
+print('Aqui temos uma variedade de planos para o aluguel de bicicletas, que são:\n')
+
+print('1 - Plano Passeio - R$ 5,00 por hora;')
 print('2 - Plano Diário - R$ 25,00 por dia;')
 print('3 - Plano Semanal - R$ 100,00 por semana e;')
 print('4 - Plano Família, que consiste em alugar de 3 a 5 bicicletas nos planos')
@@ -130,9 +146,12 @@ print('digite \"E\" para visualizar a quantidade de biciletas em estoque')
 print('ou digite 0 para cancelar.')
 
 opcao = validaEntrada(input('\nQual o plano que você deseja contratar? ').upper())
-pedidoCliente = (menuEntradas(opcao))
+menuEntradas(opcao)
 
-# Log
-print(f'A opção do cliente foi: {pedidoCliente}')
+print('\n##### LOG #####:')
+print(f'Cliente - alugarHora está retornando: {cliente.alugarHora}.')
+print(f'Cliente - alugarDia está retornando: {cliente.alugarDia}.')
+print(f'Cliente - alugarSemana está retornando: {cliente.alugarSemana}.')
+print(f'Cliente - alugarFamilia está retornando: {cliente.alugarFamilia}.')
 
 print('\nFim do programa.')
